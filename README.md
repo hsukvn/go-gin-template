82764baf1210e7eafe514ac431823c2046120064
Kevin Hsu <hsukvn@gmail.com>
2018-10-01 10:33:57 +0800

    Add ping controller

---
+++ README.md
@@ -0,0 +1,3 @@
+# go-gin-template
+
+gin http framework template with basic file hierarchy
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/hsukvn/go-gin-template/server"
+)
+
+func main() {
+	server.Init()
+}
